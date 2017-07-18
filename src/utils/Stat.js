81f6aa8d872d70b4e6ca682e6a435de1fa40be3a
zhangjk95 <zjk_261011@163.com>
2017-07-18 12:09:13 +0800

    update stat

--- src/utils/Stat.js
+++ src/utils/Stat.js
@@ -1,4 +1,5 @@
 import Config from '../Config';
+import Utils from './Utils';
 
 class Stat {
     static post(url, data = {}) {
@@ -31,6 +32,21 @@ class Stat {
     }
 
     static generate(options) {
+        var optionsConfig = Utils.arrayToObject(Config.options, item => item.key);
+        options = Object.assign({}, options);
+        Object.keys(options).forEach(key => {
+            if (!optionsConfig.hasOwnProperty(key)) {
+                delete options[key];
+            }
+            else {
+                if (optionsConfig[key].type === 'multiple') {
+                    options[key] = ['random'].concat(optionsConfig[key].options)[options[key]];
+                }
+                else {
+                    options[key] = {'-1': 'off', '0': 'random', '1': 'on'}[options[key]];
+                }
+            }
+        });
         return Stat.post('/generate', {options: options});
     }
 }
