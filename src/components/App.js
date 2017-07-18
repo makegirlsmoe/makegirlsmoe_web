af1e581ac6dd432c5c979a695ab90bd880841f16
zhangjk95 <zjk_261011@163.com>
2017-07-18 19:38:42 +0800

    update routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,17 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
-import About from './About';
 import Stat from './Stat';
 import './App.css';
 
 class App extends Component {
     render() {
-        var pathname = this.props.location.pathname;
         return (
             <div className="App">
                 <Navbar location={this.props.location}/>
                 <div className="main-content">
-                    {pathname !== '/stat' &&
-                        <div hidden={pathname !== '/'}>
-                            <Home />
-                        </div>
-                    }
                     <Switch>
-                        <Route path="/about" component={About}/>
+                        <Route path="/(|about)" component={Home}/>
                         <Route path="/stat" component={Stat}/>
                     </Switch>
                 </div>
