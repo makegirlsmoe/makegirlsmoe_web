815ded262775c65d3ee5e35fe4a2275fcb95a190
Aixile <kaguyaelf@gmail.com>
2017-08-13 05:30:06 +0900

    update pdf report link and remove old models

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ class About extends Component {
                 <p> Yanghua Jin / yanghuajin94@gmail.com (Fudan University/Preferred Networks) </p>
                 <p> Jiakai Zhang (Carnegie Mellon University)</p>
                 <h3 style={{color: Config.colors.theme}}>Technical Report</h3>
-                <a href="http://girls.moe/make_girls_moe_report.pdf" target="_blank" rel="noopener noreferrer">Create Anime Characters with A.I. ! </a> <br/>
+                <a href="http://make.girls.moe/technical_report.pdf" target="_blank" rel="noopener noreferrer">Create Anime Characters with A.I. ! </a> <br/>
                 First publised as a Doujinshi on <a href="http://www.comiket.co.jp/" target="_blank" rel="noopener noreferrer"> Comiket</a> 92, Summer 2017<br/>
                 ガールス・マニホールド　三日目東ウ05a
                 <h4 style={{color: Config.colors.theme}} >Technical Report Authors </h4>
