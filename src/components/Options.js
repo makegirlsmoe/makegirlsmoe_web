47ccaec69e3b9e12a491456a0531cec75284473a
hanezu <tim.zhc@gmail.com>
2017-08-15 00:12:54 +0900

    save noise (except condition)

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,12 +6,18 @@ import MultipleSelector from './MultipleSelector';
 import NoiseSelector from './NoiseSelector';
 import NoiseVisualizer from './NoiseVisualizer';
 import './Options.css';
+import ButtonPrimary from './ButtonPrimary';
+import PropTypes from 'prop-types';
+import copy from 'copy-to-clipboard';
 
 class Options extends Component {
 
     constructor(props) {
         super();
         this.options = Utils.arrayToObject(props.options, item => item.key);
+        this.props = {noise: '', copied: false};
+        // this.props.copied = false
+        // this.props.on
     }
 
     renderLabel(key, title) {
@@ -65,6 +71,16 @@ class Options extends Component {
             </div>
         );
     }
+    //
+    // onChange({target: {value}}) {
+    //   // this.state = {value: '', copied: false};
+    //   return this.props.copied = false
+    // }
+    //
+    // onCopy() {
+    //   // this.state = {value: '', copied: true};
+    //   return this.props.copied = true
+    // }
 
     render() {
         return (
@@ -88,6 +104,14 @@ class Options extends Component {
                 <div className="row">
                     {this.renderNoiseSelector('noise')}
                     {this.props.noise && this.renderNoiseVisualizer()}
+                    <CopyToClipboard text={this.props.noise} onCopy={() => {
+                      this.setState({copied: true});
+                    }}>
+                      <ButtonPrimary
+                        text={'Copy Noise to Clipboard'}
+                        onClick={this.props.onGenerateClick} />
+                    </CopyToClipboard>
+                    {this.props.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
                 </div>
             </div>
         );
@@ -95,3 +119,50 @@ class Options extends Component {
 }
 
 export default Options;
+
+export class CopyToClipboard extends React.PureComponent {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    children: PropTypes.element.isRequired,
+    onCopy: PropTypes.func,
+    options: PropTypes.shape({
+      debug: PropTypes.bool,
+      message: PropTypes.string
+    })
+  };
+
+  onClick = event => {
+    const {
+      text,
+      onCopy,
+      children,
+      options
+    } = this.props;
+
+    const elem = React.Children.only(children);
+
+    const result = copy(text, options);
+
+    if (onCopy) {
+      onCopy(text, result);
+    }
+
+    // Bypass onClick if it was present
+    if (elem && elem.props && typeof elem.props.onClick === 'function') {
+      elem.props.onClick(event);
+    }
+  };
+
+  render() {
+    const {
+      text: _text,
+      onCopy: _onCopy,
+      options: _options,
+      children,
+      ...props
+    } = this.props;
+    const elem = React.Children.only(children);
+
+    return React.cloneElement(elem, {...props, onClick: this.onClick});
+  }
+}
