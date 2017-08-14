525baa8b42abf85ffce8efb884f78fbdea6ff992
Aixile <kaguyaelf@gmail.com>
2017-08-15 01:03:09 +0900

    use 8bit compression

--- src/Config.js
+++ src/Config.js
@@ -74,7 +74,7 @@ var Config = {
         labelLength: 34,
         imageWidth: 128,
         imageHeight: 128,
-        model: '/models/model_resnet_with_condition_128_full_1',
+        model: '/models/model_resnet_with_condition_128_full_1_8bit',
         modelServers: debug ? ['localhost:3000'] : ['139.162.88.6']
     },
     stat: {
