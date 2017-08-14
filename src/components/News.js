43ddd33ea8887f6fa1a4f7a418ca5331c68a08ba
Yingtao Tian <alan.yt.tian@gmail.com>
2017-08-14 07:07:04 -0700

    fix link

--- src/components/News.js
+++ src/components/News.js
@@ -10,7 +10,7 @@ class News extends Component {
 
                 <p>
                   2017-08-14: We made <a href="https://makegirlsmoe.github.io/main/2017/08/14/news-english.html" target="_blank" rel="noopener noreferrer">a post in English</a> explain our work
-                  in our <a href="https://makegirlsmoe.github.io/">official blog</a>.
+                  in our <a href="https://makegirlsmoe.github.io/" target="_blank" rel="noopener noreferrer">official blog</a>.
                 </p>
 
             </div>
