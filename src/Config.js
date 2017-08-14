ecd9dcdd58a23ca8a8c33083fd895bd0b9fd4b52
Aixile <kaguyaelf@gmail.com>
2017-08-15 03:33:57 +0900

    switch to temporary server

--- src/Config.js
+++ src/Config.js
@@ -75,7 +75,7 @@ var Config = {
         imageWidth: 128,
         imageHeight: 128,
         model: '/models/model_resnet_with_condition_128_full_1',
-        modelServers: debug ? ['localhost:3000'] : ['139.162.88.6']
+        modelServers: debug ? ['localhost:3000'] : ['106.187.49.34:8080']
     },
     stat: {
         enabled: true,
