04b5ca2a2bd28d228623f93e6ac94b8b9a318b06
zhangjk95 <zjk_261011@163.com>
2017-08-18 12:26:45 -0400

    update webdnn with customized features

--- public/modules/webdnn/webdnn.js
+++ public/modules/webdnn/webdnn.js
@@ -257,7 +257,13 @@ function webdnnFetch(input, init) {
                 url: transformUrl(input.url) + ((init && init.ignoreCache) ? '?t=' + Date.now() : '')
             });
         }
-        let res = yield fetch(input, init);
+        let res;
+        if (typeof input == 'string' && isXHR2WithBlobSupported()) {
+            res = yield fetchUsingXHR(input, init && init.progressCallback);
+        }
+        else {
+            res = yield fetch(input, init);
+        }
         if (!res.ok)
             throw new Error(`Fetch returns status code ${res.status}: ${res.statusText}`);
         return res;
@@ -296,6 +302,46 @@ function readArrayBufferProgressively(res, callback) {
         }
     }
     return reader.read().then(accumulateLoadedSize);
+}
+function isXHR2WithBlobSupported() {
+    if (!window.hasOwnProperty('ProgressEvent') || !window.hasOwnProperty('FormData')) {
+        return false;
+    }
+    let xhr = new XMLHttpRequest();
+    if (typeof xhr.responseType === 'string') {
+        try {
+            xhr.responseType = 'blob';
+            return xhr.responseType === 'blob';
+        }
+        catch (e) {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+function fetchUsingXHR(url, callback) {
+    return new Promise(function (resolve, reject) {
+        let req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.responseType = "blob";
+        let callbackScheduler = new DispatchScheduler();
+        req.onload = function (event) {
+            callbackScheduler.forceDispatch();
+            let res = new Response(req.response);
+            resolve(res);
+        };
+        req.onprogress = function (event) {
+            if (callback) {
+                callbackScheduler.request(function () { return callback(event.loaded, event.total); });
+            }
+        };
+        req.onerror = function (event) {
+            reject(event);
+        };
+        req.send(null);
+    });
 }
 
 /**
@@ -513,12 +559,12 @@ class DescriptorRunnerFallback extends DescriptorRunner {
             //nothing to do
         });
     }
-    load(directory, progressCallback) {
+    load(directory, progressCallback, weightDirectory) {
         return __awaiter(this, void 0, void 0, function* () {
             let [descriptor, weightRawArray] = yield Promise.all([
-                webdnnFetch(`${directory}/graph_${this.backendName}.json`, { ignoreCache: this.ignoreCache })
+                webdnnFetch(`${directory}/graph_${this.backendName}.json`, { ignoreCache: this.ignoreCache, progressCallback: progressCallback })
                     .then(res => res.json()),
-                webdnnFetch(`${directory}/weight_${this.backendName}.bin`, { ignoreCache: this.ignoreCache })
+                webdnnFetch(`${weightDirectory || directory}/weight_${this.backendName}.bin`, { ignoreCache: this.ignoreCache })
                     .then(res => readArrayBufferProgressively(res, progressCallback))
             ]);
             this.setDescriptor(descriptor);
@@ -706,7 +752,7 @@ class DescriptorRunnerWebassembly extends DescriptorRunner {
         //nothing to do
         return Promise.resolve();
     }
-    load(directory, progressCallback) {
+    load(directory, progressCallback, weightDirectory) {
         return __awaiter(this, void 0, void 0, function* () {
             let graph_url = `${directory}/graph_${this.backendName}.json`;
             let graph_fetch = yield webdnnFetch(graph_url, { ignoreCache: this.ignoreCache });
@@ -721,8 +767,8 @@ class DescriptorRunnerWebassembly extends DescriptorRunner {
             worker_entry_js_path = transformUrl(worker_entry_js_path);
             this.worker_entry_js_path = worker_entry_js_path;
             yield this.compile();
-            let weight_url = `${directory}/weight_${this.backendName}.bin`;
-            let weight_fetch = yield webdnnFetch(weight_url, { ignoreCache: this.ignoreCache });
+            let weight_url = `${weightDirectory || directory}/weight_${this.backendName}.bin`;
+            let weight_fetch = yield webdnnFetch(weight_url, { ignoreCache: this.ignoreCache, progressCallback: progressCallback });
             let weights_data_ab = yield readArrayBufferProgressively(weight_fetch, progressCallback);
             yield this.loadWeights(new Uint8Array(weights_data_ab));
             //assign buffer to input/output buffer view
@@ -1147,12 +1193,12 @@ class DescriptorRunnerWebGPU extends DescriptorRunner {
     initializeBasicKernels() {
         this.webgpuHandler.loadKernel('kernel void sync(){}', 'basic');
     }
-    load(directory, progressCallback) {
+    load(directory, progressCallback, weightDirectory) {
         return __awaiter(this, void 0, void 0, function* () {
             let [descriptor, weightRawArray] = yield Promise.all([
                 webdnnFetch(`${directory}/graph_${this.backendName}.json`, { ignoreCache: this.ignoreCache })
                     .then(res => res.json()),
-                webdnnFetch(`${directory}/weight_${this.backendName}.bin`, { ignoreCache: this.ignoreCache })
+                webdnnFetch(`${weightDirectory || directory}/weight_${this.backendName}.bin`, { ignoreCache: this.ignoreCache, progressCallback: progressCallback })
                     .then(res => readArrayBufferProgressively(res, progressCallback))
             ]);
             yield this.setDescriptor(descriptor);
@@ -2160,7 +2206,7 @@ function load(directory, initOption = {}) {
                 continue;
             runner.ignoreCache = Boolean(initOption.ignoreCache);
             try {
-                yield runner.load(directory, initOption.progressCallback);
+                yield runner.load(directory, initOption.progressCallback, initOption.weightDirectory);
             }
             catch (ex) {
                 console.warn(`Model loading failed for ${backendName} backend. Trying next backend: ${ex.message}`);
