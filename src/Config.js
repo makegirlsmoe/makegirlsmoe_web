751363f5a57539975253313fd3d767431d2313cf
Aixile <amane.nagatsuki@gmail.com>
2017-10-11 16:05:05 +0900

    Remove china cdn

--- src/Config.js
+++ src/Config.js
@@ -83,7 +83,6 @@ var Config = {
                 imageHeight: 128,
                 model: '/models/Amaryllis',
                 modelServers: debug ? ['localhost:3000'] : [
-                    {host: 'girls.moe', country: 'CN'},
                     'make.girls.moe'
                 ]
             },
