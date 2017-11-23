0dc1a0aac010739ea07b8514a902ad81db22aab3
Aixile <amane.nagatsuki@gmail.com>
2017-11-23 17:38:23 +0900

    update about

--- src/components/pages/License.js
+++ src/components/pages/License.js
@@ -7,13 +7,13 @@ class License extends Component {
             <div className="license">
                 <h3 style={{color: Config.colors.theme}}>License</h3>
                 <p>To use MakeGirlsMoe, you must agree with the following licenses.</p>
-                <p>MakeGirlsMoe is freely available only for non-commercial use. Please, see the license for further details. For commercial queries, contact Yanghua Jin.</p>
+                <p>MakeGirlsMoe is freely available only for non-commercial use. Please, see the license for further details. For commercial queries, contact  <a href="mailto:yanghuajin94@gmail.com" target="_blank" rel="noopener noreferrer">Yanghua Jin</a>.</p>
 
                 <p>The code of web interface is under the GPL v3.0 license and can be redistributed.</p>
                 <p>All the compiled model files are privately owned by Yanghua Jin and are not allowed for unauthorized commercial use.</p>
                 <p></p>
                 <p>使用MakeGirlsMoe前请注意以下条款</p>
-                <p>MakeGirlsMoe可以免费用于任何非商业用途，请在这里查看具体使用许可。对于任何商业请求与咨询，请联系Yanghua Jin。</p>
+                <p>MakeGirlsMoe可以免费用于任何非商业用途，请在这里查看具体使用许可。对于任何商业请求与咨询，请联系<a href="mailto:yanghuajin94@gmail.com" target="_blank" rel="noopener noreferrer">Yanghua Jin</a>。</p>
 
                 <p>网站前段源代码部分使用GPL v3.0许可，可以在遵守相关许可的情况下重新发放。</p>
                 <p>所有的预训练模型相关的文件，都由Yanghua Jin所私有，请勿在未经过许可的情况下进行任何商业使用。</p>
